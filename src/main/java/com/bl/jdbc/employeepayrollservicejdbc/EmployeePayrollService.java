5e83d73ca70c7c4bacfe6105549e9e3737aaec11
Jagriti <jagriti.agni@gmail.com>
2020-10-29 21:40:05 +0530

    [Jagriti]ADDED: Print welcome message

---
+++ src/main/java/com/bl/jdbc/employeepayrollservicejdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bl.jdbc.employeepayrollservicejdbc;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee payroll service");
+	}
+}
