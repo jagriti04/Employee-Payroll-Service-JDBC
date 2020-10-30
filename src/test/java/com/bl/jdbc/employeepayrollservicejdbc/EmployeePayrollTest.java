75a3f9c333a36f2d179ab40907d38dc96a4e4b21
Jagriti <jagriti.agni@gmail.com>
2020-10-30 23:15:28 +0530

    [Jagriti]ADDED: Test case to check if salary updated in database

--- src/test/java/com/bl/jdbc/employeepayrollservicejdbc/EmployeePayrollTest.java
+++ src/test/java/com/bl/jdbc/employeepayrollservicejdbc/EmployeePayrollTest.java
@@ -16,4 +16,13 @@ public class EmployeePayrollTest {
 				.readEmployeePayrollDataDB(EmployeePayrollService.IOService.DB_IO);
 		Assert.assertEquals(3, empPayrollData.size());
 	}
+	
+	@Test
+	public void givenNewSalaryForEmployee_whenUpdated_shouldSyncWithDB() {
+		EmployeePayrollService empPayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> empPayrollData = empPayrollService.readEmployeePayrollDataDB(EmployeePayrollService.IOService.DB_IO);
+		empPayrollService.updateEmployeeSalary("Terisa", 3000000.0);
+		boolean result = empPayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+		Assert.assertTrue(result);
+	}
 }
